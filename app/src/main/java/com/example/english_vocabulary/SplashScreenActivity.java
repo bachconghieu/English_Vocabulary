a852836f36ff9f74cf2e992e91b8dba0a8361064
admin <hieubvph14441@fpt.edu.vn>
2021-11-25 10:03:07 +0700

    hieu

---
+++ app/src/main/java/com/example/english_vocabulary/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.english_vocabulary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        },1500);
+
+    }
+}
